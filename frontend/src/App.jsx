050f0b29c95514187fdaae66a60abdf6eb43d9e4
Pranav Vadakkathiyil <vpranav0801@gmail.com>
2024-12-15 23:05:32 +0530

    App jsx changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
-        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/contact' element={<Contact/>}/> 
         <Route path='/collection' element={<Collection/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/product/:productId' element={<Product/>}/>
